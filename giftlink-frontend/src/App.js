603eb59b443444b89e409b8a6a4c45b6788a9856
Ravindre Ramjiawan <reramjiawan@users.noreply.github.com>
2025-07-22 10:47:11 +0200

    Remove unsed hook

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import MainPage from "./components/MainPage/MainPage";
 import LoginPage from "./components/LoginPage/LoginPage";
 import RegisterPage from "./components/RegisterPage/RegisterPage";
@@ -9,7 +9,6 @@ import Navbar from "./components/Navbar/Navbar";
 import SearchPage from "./components/SearchPage/SearchPage";
 
 function App() {
-  const navigate = useNavigate();
   return (
     <>
       <Navbar />
